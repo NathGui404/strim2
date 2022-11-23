3d70036f0030c9c8bc08c92f3baa59a2deb05f63
Nathor <110332997+NathGui404@users.noreply.github.com>
2022-11-23 11:05:35 +0100

    Add files via upload

---
+++ my_streamlit_app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+
+st.title('On va voir des voitures')
+st.write("j'adore les voitures")
+
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df_cars = pd.read_csv(link)
+
+viz_correlation = sns.heatmap(df_cars.corr(), 
+								center=0,
+								cmap = sns.color_palette("vlag", as_cmap=True)
+								)
+
+st.pyplot(viz_correlation.figure)
