ff9e754ef97b11f4eea4caa0396bc892ec9a9606
Nathor <110332997+NathGui404@users.noreply.github.com>
2022-11-25 12:54:49 +0100

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -3,22 +3,25 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 
-st.title('On va voir des voitures')
-st.write("j'adore les voitures")
+
 # radio button
 # first argument is the title of the radio button
 # second argument is the options for the ratio button
 status = st.radio("Quel est ton formateur préféré: ", ('Pierre', 'Autre'))
  
+st.image(pierre_mur.jpg, width=200)
 # conditional statement to print
 # Male if male is selected else print female
 # show the result using the success function
 if (status == 'Pierre'):    
 	st.success("Pierre Evidemment")
-	st.image(pierre_mur.jpg, width=200)
+	
 else:
     st.success("Est tu sûr de ton choix?")
 
+st.title('Même si aucun rapport avec le début, on va étudier des correlations avec des voitures')
+st.write("j'adore les voitures")
+st.write("jamais une heatmap n'aura été si belle")
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_cars = pd.read_csv(link)
 
