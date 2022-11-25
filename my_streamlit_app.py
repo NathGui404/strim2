a905baedd2b0bf5a9af884ef85c92cf00a30bf7e
Nathor <110332997+NathGui404@users.noreply.github.com>
2022-11-25 13:48:19 +0100

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -6,21 +6,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
+import PIL
+from PIL import Image
 
 
 # radio button
 # first argument is the title of the radio button
 # second argument is the options for the ratio button
-st.title("Quel est ton formateur préféré: ")
 status =st.radio(
     "Quel est ton formateur préféré?",
     ('Autre','Pierre'))
 # conditional statement to print
 # show the result using the success function
-
+image = Image.open('pierre_mur.jpg')
 if (status == 'Pierre'):    
 	st.success("C'était évident !")
-	st.image(pierre_mur.jpg, width=200)
+	st.image(image, width=200)
 else:
 	st.warning("Est tu sûr de ton choix?")
 
