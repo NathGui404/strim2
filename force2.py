24cd93677b4de355d9d5c7e71a4d4c26db56247b
Nathor <110332997+NathGui404@users.noreply.github.com>
2022-11-28 06:37:47 +0100

    Update force2.py

--- force2.py
+++ force2.py
@@ -8,21 +8,25 @@ Created on Sun Nov 27 15:31:18 2022
 import streamlit as st
 import pandas as pd
 import seaborn as sns
+from PIL import Image
 
 st.title('Toi tu aimes les voitures!')
 
+image = Image.open('https://github.com/NathGui404/strim/blob/0a671af202beb8704005c256821ba22efe339ea7/bugatti.jpg')
+
+st.image(image, caption='Bugatti')
 st.write("Tu vas voir ce que tu vas voir !")
 
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_cars = pd.read_csv(link)
-df_cars['continent']=df_cars['continent'].str.strip('123.!? \n\t')
+df_cars['continent'] = df_cars['continent'].str.strip('123.!? \n\t')
 
-continent_liste=np.append(df_cars['continent'].unique(),'All') 
+continent_liste = np.append(df_cars['continent'].unique(),  'All')
 
-continent=st.radio(
+continent = st.radio(
     "select a continent",
     continent_liste,
-    key=1
+    key=1)
 
 
 if st.button('US'):
@@ -35,7 +39,7 @@ else:
 
 
 
-viz_correlation = sns.heatmap(df_cars.corr(), 
+viz_correlation = sns.heatmap(df_cars.corr(),
 								center=0,
 								cmap = sns.color_palette("vlag", as_cmap=True),
                                 annot=True
@@ -43,10 +47,11 @@ viz_correlation = sns.heatmap(df_cars.corr(),
 
 st.pyplot(viz_correlation.figure)
 
-  # graph 2
-   # distance parcourue par gallon consommée - Plus la voiture est lourde, moins elle fait de miles
+# graph 2
+# distance parcourue par gallon consommée - Plus la voiture est lourde, moins elle fait de miles
+
 fig = plt.figure(figsize=(12, 9))
-fig = px.scatter(data_frame = df_cars,
+fig = px.scatter(data_frame=df_cars,
                    y= "Miles per gallon_(mpg)", x="Weight_Lbs",
                    color="Continent",
                    )
@@ -57,4 +62,4 @@ fig.update_layout(title ="bDistance parcourue en fonction du poids des véhicule
                    height=700,
                    hovermode='closest',
                    template='plotly_dark')
-st.plotly_chart(fig, config = config, use_container_width = True)
+st.plotly_chart(fig, config=config, use_container_width=True)
